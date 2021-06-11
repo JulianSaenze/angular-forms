6501c2a31c3e4a534721ebd27af6164aa61ce9ee
Julian Saenze <julian.saenze@comsysto.com>
2021-06-11 09:53:43 +0200

    setting and patching form values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,8 +7,29 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('f') signUpForm: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    //set the value of the whole form (not the best approach)
+    // this.signUpForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+
+    //better approach -> patchValue to overwrite parts of the form and setValue to set the whole form
+    this.signUpForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }});
   }
 
   //triggered every time that form is submitted
